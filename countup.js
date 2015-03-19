f7d23c895ecddda2a136c0b8ed923472bc3c92fd
quantumlicht <guay.philippe@gmail.com>
2015-03-19 00:28:40 -0400

    fixing countup

--- countup.js
+++ countup.js
@@ -1,131 +1,112 @@
-/**
- * @name		jQuery Count-UP Plugin
- * @author		Martin Angelov
- * @version 	1.0
- * @url			http://tutorialzine.com/2012/09/count-up-jquery/
- * @license		MIT License
- */
-
 (function(){
 
-
-
-	// Creating the plugin
 	countup = function(prop){
 
 		// Number of seconds in every time division
-		minute_to_sec	= 60;
-		hour_to_sec	= 60 * minute_to_sec;
-		day_to_sec	= 24 * hour_to_sec;
-		year_to_sec = 365 * day_to_sec;
+		m2sec	= 60;
+		h2sec	= 60 * m2sec;
+		d2sec	= 24 * h2sec;
+		y2sec = 365 * d2sec;
 
 		options = _.extend({
 			callback	: function(){},
 			start		: new Date(),
-			format: '%Y:%M:%d:%h:%m:%s'
+			format: '%Y::%M::%d:%h:%m:%s',
+			debug: true
 		},prop);
 
-		var passed = 0, y, M, d, h, m, s;
-		var yearpassed_in_secs = 0;
+		if (!options.debug) {
+			console.log = function(){}
+		}
 
-		// Initialize the plugin
-		// init(this, options);
+		var passed = 0, Y, M, d, h, m, s;
+
 		(function tick() {
-			console.log('============ NEW TICK ==========');
-			console.log('== ' + options.start.toString() + ' ==');
-			console.log('============ NEW TICK ==========');
 			now = new Date();
-
+			var _trail = _.cloneDeep(options.start);
 			passed = Math.floor((now - options.start) / 1000);
+			console.log('passed in days', passed / d2sec);
 
 			// YEARS
 			if (options.format.indexOf('%Y') > -1) {
-				y = now.getFullYear() - options.start.getFullYear() - 1;
-				var dayOffset = 0;
-				for (var i=0; i < y; i++) {
+				var Y_passed_in_sec;
+				Y = now.getFullYear() - options.start.getFullYear() - 1;
+				var leapYearOffset = 0;
+				for (var i=0; i < Y; i++) {
 					if (isLeapYear(options.start.getFullYear() + i)){
-						dayOffset ++;
+						leapYearOffset ++;
 					}
 				}
-				yearpassed_in_secs = y*year_to_sec - dayOffset * day_to_sec;
-				passed -= yearpassed_in_secs;
+				Y_passed_in_sec = Y * y2sec + leapYearOffset * d2sec;
+				passed -= Y_passed_in_sec;
 			}
-
 
 			// MONTHS
 			if (options.format.indexOf('%M') > -1) {
-				M = getMonthDiff(options.start, now);
-				if (y) {
-					M -= y*12;
-				}
-				var monthsInDays = 0,
-						startYear    = options.start.getFullYear() + y,
-						startMonth   = 0;
 
-				for (var i=0; i < M; i++) {
-					year = startYear + Math.floor(i/12)
-					month = (startMonth + i)%12
-					console.log('month', month)
-					monthsInDays += daysInMonth(year, month);
+				_trail.setTime(_trail.getTime() + Y_passed_in_sec * 1000)
+				console.log('Month_trail', _trail.toString());
+
+				M = getMonthDiff(_trail, now);
+				var m_init = _trail.getMonth();
+				var y_init = _trail.getFullYear();
+
+				var M_passed_in_sec = daysInMonth(m_init, y_init) - _trail.getDate();
+				for (var _idx=m_init + 1; _idx <= m_init + M; _idx++) {
+					M_passed_in_sec += daysInMonth(_idx, y_init) * d2sec;
 				}
-				console.log('monthsInDays', monthsInDays);
-				passed -= (monthsInDays * day_to_sec);
+				passed -= M_passed_in_sec
 			}
 
 			// DAYS
 			if (options.format.indexOf('%d') > -1) {
-				d = Math.floor(passed / day_to_sec);
-				// updateDuo(0, 1, d);
-				passed -= d*day_to_sec;
+				_trail.setTime(_trail.getTime() + M_passed_in_sec * 1000);
+				console.log('Day_trail', _trail.toString());
+
+				d = Math.floor((now.getTime() -_trail.getTime())/(1000*d2sec));
+				d_passed_in_sec = d*d2sec;
+				passed -= d_passed_in_sec;
 			}
 
 			// HOURS
 			if (options.format.indexOf('%h') > -1) {
-				h = Math.floor(passed / hour_to_sec);
-				// updateDuo(2, 3, h);
-				passed -= h*hour_to_sec;
+				_trail.setTime(_trail.getTime() + d_passed_in_sec * 1000);
+				console.log('Hour_trail', _trail.toString());
+
+				h = Math.floor(passed / h2sec);
+				h_passed_in_sec = h*h2sec
+				passed -= h*h2sec;
 			}
 
 			// MINUTES
 			if (options.format.indexOf('%m') > -1) {
-				m = Math.floor(passed / minute_to_sec);
-				// updateDuo(4, 5, m);
-				passed -= m*minute_to_sec;
+				_trail.setTime(_trail.getTime() + h_passed_in_sec * 1000);
+				console.log('Min_trail', _trail.toString());
+				m = Math.floor((now.getTime() -_trail.getTime())/(1000*m2sec));
+				passed -= m * m2sec;
 			}
 
 			// SECONDS
 			if (options.format.indexOf('%m') > -1) {
 				s = passed;
-				// updateDuo(6, 7, s);
 			}
-
-			console.log(y,M,d,h,m,s);
-			var formattedStr = getFormattedCountUp(y,M,d,h,m,s);
-			// Calling an optional user supplied callback
-			console.log('formattedStr', formattedStr)
+			var formattedStr = getFormattedCountUp(Y,M,d,h,m,s);
 			options.callback(formattedStr);
 
-			// Scheduling another call of this function in 1s
 			setTimeout(tick, 1000);
 		})();
-
-		// This function updates two digit positions at once
-		// function updateDuo(minor,major,value){
-		// 	switchDigit(positions.eq(minor),Math.floor(value/10)%10);
-		// 	switchDigit(positions.eq(major),value%10);
-		// }
 
 		return this;
 	};
 
-	function getFormattedCountUp(y, m, d, h, m, s){
+	function getFormattedCountUp(y, M, d, h, m, s){
 		var str = options.format
-		str = str.replace('%Y', y);
-		str = str.replace('%M', m);
-		str = str.replace('%d', d);
-		str = str.replace('%h', h);
-		str = str.replace('%m', m);
-		str = str.replace('%s', s);
+		str = str.replace("%Y", y);
+		str = str.replace("%M", M);
+		str = str.replace("%d", d);
+		str = str.replace("%h", h);
+		str = str.replace("%m", m);
+		str = str.replace("%s", s);
 		return str;
 	}
 
@@ -136,74 +117,14 @@
 	function getMonthDiff(d1, d2) {
     var months;
     months = (d2.getFullYear() - d1.getFullYear()) * 12;
-    months -= d1.getMonth() + 1;
+    months -= d1.getMonth();
     months += d2.getMonth();
     return months <= 0 ? 0 : months;
 	}
 
-	function daysInMonth(month,year) {
-    return new Date(year, month, 0).getDate();
+	function daysInMonth(month, year) {
+		var days = new Date(year, month, 0).getDate();
+    return days;
 	}
-	// function init(elem, options){
-	// 	elem.addClass('countdownHolder');
 
-	// 	// Creating the markup inside the container
-	// 	$.each(['Days','Hours','Minutes','Seconds'],function(i){
-	// 		$('<span class="count'+this+'">').html(
-	// 			'<span class="position">\
-	// 				<span class="digit static">0</span>\
-	// 			</span>\
-	// 			<span class="position">\
-	// 				<span class="digit static">0</span>\
-	// 			</span>'
-	// 		).appendTo(elem);
-
-	// 		if(this!="Seconds"){
-	// 			elem.append('<span class="countDiv countDiv'+i+'"></span>');
-	// 		}
-	// 	});
-
-	// }
-
-	// // Creates an animated transition between the two numbers
-	// function switchDigit(position,number){
-
-	// 	var digit = position.find('.digit')
-
-	// 	if(digit.is(':animated')){
-	// 		return false;
-	// 	}
-
-	// 	if(position.data('digit') == number){
-	// 		// We are already showing this number
-	// 		return false;
-	// 	}
-
-	// 	position.data('digit', number);
-
-	// 	var replacement = $('<span>',{
-	// 		'class':'digit',
-	// 		css:{
-	// 			top:'-2.1em',
-	// 			opacity:0
-	// 		},
-	// 		html:number
-	// 	});
-
-	// 	// The .static class is added when the animation
-	// 	// completes. This makes it run smoother.
-
-	// 	digit
-	// 		.before(replacement)
-	// 		.removeClass('static')
-	// 		.animate({top:'2.5em',opacity:0},'fast',function(){
-	// 			digit.remove();
-	// 		})
-
-	// 	replacement
-	// 		.delay(100)
-	// 		.animate({top:0,opacity:1},'fast',function(){
-	// 			replacement.addClass('static');
-	// 		});
-	// }
 })();
