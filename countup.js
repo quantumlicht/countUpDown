e746f0249b10c61c50e1c50eb607d72071f2b017
quantumlicht <guay.philippe@gmail.com>
2015-03-18 22:19:58 -0400

    small improvements

--- countup.js
+++ countup.js
@@ -14,16 +14,15 @@
 	countup = function(prop){
 
 		// Number of seconds in every time division
-		var year_to_sec = 365*60*60,
-			month_to_sec = 365*60*60,
-			day_to_sec	= 24*60*60,
-			hour_to_sec	= 60*60,
-			minute_to_sec	= 60;
+		minute_to_sec	= 60;
+		hour_to_sec	= 60 * minute_to_sec;
+		day_to_sec	= 24 * hour_to_sec;
+		year_to_sec = 365 * day_to_sec;
 
-		var options = _.extend({
+		options = _.extend({
 			callback	: function(){},
 			start		: new Date(),
-			format: 'Y:M:d:h:m:s'
+			format: '%Y:%M:%d:%h:%m:%s'
 		},prop);
 
 		var passed = 0, y, M, d, h, m, s;
@@ -32,64 +31,79 @@
 		// Initialize the plugin
 		// init(this, options);
 		(function tick() {
+			console.log('============ NEW TICK ==========');
+			console.log('== ' + options.start.toString() + ' ==');
+			console.log('============ NEW TICK ==========');
 			now = new Date();
 
 			passed = Math.floor((now - options.start) / 1000);
 
 			// YEARS
-			if (options.format.indexOf('Y') > -1) {
-				y = now.getFullYear() - options.start.getFullYear();
+			if (options.format.indexOf('%Y') > -1) {
+				y = now.getFullYear() - options.start.getFullYear() - 1;
 				var dayOffset = 0;
 				for (var i=0; i < y; i++) {
 					if (isLeapYear(options.start.getFullYear() + i)){
 						dayOffset ++;
 					}
 				}
-				year_p
-				yearpassed_in_secs = d*year_to_sec - dayOffset * day_to_sec;
+				yearpassed_in_secs = y*year_to_sec - dayOffset * day_to_sec;
 				passed -= yearpassed_in_secs;
 			}
 
 
 			// MONTHS
-			if (options.format.indexOf('M') > -1) {
+			if (options.format.indexOf('%M') > -1) {
 				M = getMonthDiff(options.start, now);
+				if (y) {
+					M -= y*12;
+				}
 				var monthsInDays = 0,
-						startYear    = options.start.getFullYear(),
-						startMonth   = options.start.getMonth();
+						startYear    = options.start.getFullYear() + y,
+						startMonth   = 0;
 
 				for (var i=0; i < M; i++) {
 					year = startYear + Math.floor(i/12)
-					month = (startMonth + i)%12 + 1
+					month = (startMonth + i)%12
+					console.log('month', month)
 					monthsInDays += daysInMonth(year, month);
 				}
-
-				passed -= monthsInDays * day_to_sec;
+				console.log('monthsInDays', monthsInDays);
+				passed -= (monthsInDays * day_to_sec);
 			}
 
-			console.log('passed after months', passed);
-
 			// DAYS
-			d = Math.floor(passed / day_to_sec);
-			// updateDuo(0, 1, d);
-			passed -= d*day_to_sec;
+			if (options.format.indexOf('%d') > -1) {
+				d = Math.floor(passed / day_to_sec);
+				// updateDuo(0, 1, d);
+				passed -= d*day_to_sec;
+			}
 
 			// HOURS
-			h = Math.floor(passed / hour_to_sec);
-			// updateDuo(2, 3, h);
-			passed -= h*hour_to_sec;
+			if (options.format.indexOf('%h') > -1) {
+				h = Math.floor(passed / hour_to_sec);
+				// updateDuo(2, 3, h);
+				passed -= h*hour_to_sec;
+			}
 
 			// MINUTES
-			m = Math.floor(passed / minute_to_sec);
-			// updateDuo(4, 5, m);
-			passed -= m*minute_to_sec;
+			if (options.format.indexOf('%m') > -1) {
+				m = Math.floor(passed / minute_to_sec);
+				// updateDuo(4, 5, m);
+				passed -= m*minute_to_sec;
+			}
 
 			// SECONDS
-			s = passed;
-			// updateDuo(6, 7, s);
+			if (options.format.indexOf('%m') > -1) {
+				s = passed;
+				// updateDuo(6, 7, s);
+			}
 
+			console.log(y,M,d,h,m,s);
+			var formattedStr = getFormattedCountUp(y,M,d,h,m,s);
 			// Calling an optional user supplied callback
-			options.callback(y,M,d, h, m, s);
+			console.log('formattedStr', formattedStr)
+			options.callback(formattedStr);
 
 			// Scheduling another call of this function in 1s
 			setTimeout(tick, 1000);
@@ -103,6 +117,17 @@
 
 		return this;
 	};
+
+	function getFormattedCountUp(y, m, d, h, m, s){
+		var str = options.format
+		str = str.replace('%Y', y);
+		str = str.replace('%M', m);
+		str = str.replace('%d', d);
+		str = str.replace('%h', h);
+		str = str.replace('%m', m);
+		str = str.replace('%s', s);
+		return str;
+	}
 
 	function isLeapYear(year) {
   	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
